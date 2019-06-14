33e08d6ea4d95f3d6e5e3a8b7ac8a82808e193bf
IvanEstra <Iv0499@hotmail.com>
2019-06-14 14:23:06 -0500

    Sistema

---
+++ src/views/Login.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+class Login extends Component {
+    render() {
+        return (
+           
+                    <form>
+                        <div className="container">
+                        <div className="row justify-content-center">
+
+                            <div className="form-group col-md-7">
+                            <label for="InputEmail4">Correo Electronico</label>
+                            <input type="email" class="form-control" id="InputEmail1"  placeholder="Enter email"/>
+                            
+                            </div>
+                            <div className="form-group col-md-7">
+                            <label for="InputPassword4">Password</label>
+                            <input type="password" class="form-control" id="InputPassword1" placeholder="Password"/>
+                            </div>
+                    </div>
+                            <div align="center">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                            </div>
+                    </div>
+                </form>
+           
+        );
+     }
+    }
+    export default Login;
